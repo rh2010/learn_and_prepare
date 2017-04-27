0e9b391b5cceb7864c1df4f599f0095ae5a1f200
Rohit <hooda.rohit@gmail.com>
2017-04-27 09:59:14 -0700

    minor updates to readf.c

--- c/readf.c
+++ c/readf.c
@@ -4,6 +4,12 @@
 #define OUTPUT_FILE "./ofile"
 
 #define MAGIC	0xdeadbeef
+
+/*
+ * This code doesn't work as of now.
+ *
+ * TODO: Fix the code to perform the actual read and write from and to the file.
+ */
 int
 main(void)
 {
@@ -14,12 +20,15 @@ main(void)
 	ifd = open(INPUT_FILE, O_RDONLY);
 	if (ifd == -1) {
 		printf("Unable to open file %s, error:%s \n", INPUT_FILE, strerror(errno));
+		exit(-1);
 	}
 	printf("Input file %s, opened!\n", INPUT_FILE);
 
 	ofd = open(OUTPUT_FILE, (O_WRONLY|O_TRUNC));
 	if (ofd == -1) {
 		printf("Unable to open file %s, error:%s \n", OUTPUT_FILE, strerror(errno));
+		close(ifd);
+		exit(-1);
 	}
 	printf("Output file %s, opened!\n", OUTPUT_FILE);
 
