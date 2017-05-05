dbe713355173fb64574550bb3bb214b3a21d2d55
Rohit <hooda.rohit@gmail.com>
2017-05-05 11:48:49 -0700

    updating twoSum.py to use try/catch semantics. Also, make it return a list of valid pairs instead of just the last valid pair.

--- python/twoSum.py
+++ python/twoSum.py
@@ -6,23 +6,41 @@ import time
 import re
 
 
+#
+# Return two lists
+#   - First list contains all the pairs which sum equals to 'num'
+#   - Second list contains index of all the pairs which sum equals to 'num'
+#
 def twoSum(arr, num):
     hashmap = {}
+    index_list = []
+    pairs_list = []
 
-    idx1 = 0
-    idx2 = 0
+    print("List: %s, sum to: %d" % (arr, num))
 
+    idx = 0
     for val in arr:
         temp = num - val
 
-        if temp in hashmap.keys():
-            print("%d, %d\n" % (temp, val))
-            idx1 = temp
-            idx2 = val
-        else:
-            hashmap.update({val:val})
+        try:
+            index = hashmap[temp]
+            # if temp is present in the hash then temp and val are the two
+            # values whose sum is equal to 'num'
+            index_list.append([index, idx])
+            pairs_list.append([temp, val])
+        except KeyError:
+            hashmap.update({val:idx})
 
-    return idx1, idx2
+        idx = idx + 1
+
+        #if temp in hashmap.keys():
+        #    print("%d, %d\n" % (temp, val))
+        #    idx1 = temp
+        #    idx2 = val
+        #else:
+        #    hashmap.update({val:val})
+
+    return index_list, pairs_list
 
 
 
@@ -32,8 +50,9 @@ if __name__ == "__main__":
     arr = [1, 4, 45, 6, 10, -8]
     num = 16
 
-    idx1, idx2 = twoSum(arr, num)
+    list_index, list_pairs = twoSum(arr, num)
 
-    print("Index is: %d, %d\n" % (idx1, idx2))
+    print("List of index: %s" % (list_index))
+    print("List of pairs: %s" % (list_pairs))
 
     ## End main
