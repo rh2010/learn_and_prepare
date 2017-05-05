14f4e8c850c8b6efd313277878e943a92d55c26f
Rohit <hooda.rohit@gmail.com>
2017-05-05 15:08:10 -0700

    Update README for python. Add dummy file for matrix rotation (finish soon).

---
+++ c/matrix/matrix_rotate.c
@@ -0,0 +1,17 @@
+#include "../util.h"
+
+/*
+ * Implement a program to inplace right rotate (by 90 degrees) a square matrix
+ * of side n (i.e. N x N matrix).
+ */
+void
+right_rotate_90(int arr[][], int size)
+{
+
+}
+
+int
+main()
+{
+
+}
