719db25eddc9b89a7caf93ad4f7f91cb3cdfc7d8
Rohit <hooda.rohit@gmail.com>
2017-05-05 00:57:24 -0700

    Dummy implementation of graphs data structure.

---
+++ c/graphs/graph.c
@@ -0,0 +1,11 @@
+#include "../my_header.h"
+
+/*
+ * Implement:
+ * 	- Graph creation (a.k.a. graph addition)
+ * 	- Deletion from a graph
+ * 	- Search a graph
+ * 		- DFS
+ * 		- BFS
+ *
+ */
---
+++ c/graphs/graph.h
@@ -0,0 +1,9 @@
+#include "../my_header.h"
+
+/*
+ * Adjacency list to represent graphs.
+ */
+
+/*
+ * Adjacency matrix to represent graphs.
+ */
