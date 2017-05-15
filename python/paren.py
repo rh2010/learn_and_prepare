d79e60da2a7e38fd9d38d8c49ac48db05a1f711f
Rohit Hooda <hooda.rohit@gmail.com>
2017-05-15 00:32:34 -0700

    making paren.py more efficient

--- python/paren.py
+++ python/paren.py
@@ -52,6 +52,55 @@ def checkBalancedParenthesis(expr):
 
     ## End checkBalancedParenthesis()
 
+def balancedParenthesis(expr):
+    paren_pairs = {']':'[',
+                   '}':'{',
+                   ')':'(',
+                   }
+
+    print expr
+    # List of open parenthesis
+    list_open = paren_pairs.values()
+
+    # List of closed parenthesis
+    list_close = paren_pairs.keys()
+
+
+    # An empty list - use as a stack
+    stack = []
+
+    # no sanity checking
+    for c in expr:
+        if c in list_open or c in list_close:
+            try:
+                close = paren_pairs[c]
+                # c is closed parenthesis
+                # There should be a matching open brace right at the top of the
+                # stack.
+                #
+                # - If the stack is EMPTY, then return FALSE.
+                # - If the top element of the stack does not match with the
+                #   corresponding open parenthesis then, return FALSE.
+                #
+                if len(stack) == 0:
+                    return False
+                match = stack.pop()
+                if match != close:
+                    return False
+            except KeyError:
+                # c is open parenthesis
+                stack.append(c)
+            # end for
+
+    # Once the complete expression is processed, there is be nothing present
+    # on the stack.
+    if (len(stack) == 0):
+        return True
+    else:
+        return False
+
+    ## End checkBalancedParenthesis()
+
 if __name__ == "__main__":
     expr = "[()]{}{[()()]()}"
 
@@ -65,5 +114,6 @@ if __name__ == "__main__":
     print("Expression %s is balanced %s\n" % (expr2, checkBalancedParenthesis(expr2)))
     print("Expression %s is balanced %s\n" % (expr3, checkBalancedParenthesis(expr3)))
     print("Expression %s is balanced %s\n" % (expr4, checkBalancedParenthesis(expr4)))
+    print("Expression %s is balanced %s\n" % (sys.argv[1], balancedParenthesis(sys.argv[1])))
 
     ## End main
