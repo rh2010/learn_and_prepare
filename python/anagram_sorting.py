70a051140b58bbf4f2cbb4bed7819af9dc4f0918
Rohit <hooda.rohit@gmail.com>
2017-05-19 11:35:35 -0700

    Implement anagram sorting.

---
+++ python/anagram_sorting.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+import sys
+
+#
+# Given a list of strings, write a function to sort / re-arrange the
+# list in such a way that anagrams get grouped together.
+#
+def calc_hash(word):
+    res = 0
+
+    for c in word:
+        res = res ^ ord(c)
+
+    print("Word: %s, hash: %d" % (word, res))
+    return res
+    # End calc_hash()
+
+def group_anagrams(str_list):
+    print ("Input: \n%s" % str_list)
+    sort_dict = {}
+    word_hash = 0
+    ana_sorted_list = []
+
+    for word in str_list:
+        word_hash = calc_hash(word)
+
+        print("Adding: %s" % (word))
+        try:
+            ana_list = sort_dict[word_hash]
+
+            # something already exists in the list for this hash, update it
+            print("First time")
+            ana_list.append(word)
+            sort_dict.update({word_hash:ana_list})
+        except KeyError:
+            # first time, just add a one element list
+            print("Not First time")
+            ana_list = [word]
+            sort_dict.update({word_hash:ana_list})
+
+        print("Updated sort_dist: %s" % (sort_dict))
+
+    for key in sort_dict.keys():
+        l = sort_dict[key]
+        ana_sorted_list = ana_sorted_list + l
+
+    return ana_sorted_list
+    # End anagram_sort()
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+
+    string_list = sys.argv[1:]
+
+    anagram_sorted_list = group_anagrams(string_list)
+
+    print("The sorted list: \n%s" % (anagram_sorted_list))
