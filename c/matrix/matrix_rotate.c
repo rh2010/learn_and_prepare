ef8e60a8d120b5c03606871dc6c7486228531718
Rohit <hooda.rohit@gmail.com>
2018-03-16 07:14:17 -0700

    matrix_rotate.c: Implement Left Rotation of a matrix by 90 degrees.

--- c/matrix/matrix_rotate.c
+++ c/matrix/matrix_rotate.c
@@ -22,26 +22,34 @@ print_matrix(int arr[N][N], int size)
 }
 
 void
+left_rotate(int a[N][N], int size)
+{
+	int i, j, temp;
+
+	for (i=0; i<size/2; i++) {
+		for(j=i; j < size - i - 1; j++) {
+			temp = a[i][j];
+			a[i][j] = a[j][size-i-1];
+			a[j][size-i-1] = a[size-i-1][size-j-1];
+			a[size-i-1][size-j-1] = a[size-j-1][i];
+			a[size-j-1][i] = temp;
+		}
+	}
+}
+
+void
 right_rotate_90(int a[N][N], int size)
 {
 	int i, j;
 	int temp;
 
-	printf("right_rotate_90\n");
-
 	for (i = 0; i < size/2; i++) {
-		printf("i: %d\n", i);
 		for (j = i; j < size -i -1; j++) {
-			printf("J: %d\n", j);
-
 			temp 			= a[i][j];
 			a[i][j] 		= a[N-j-1][i];
 			a[N-j-1][i] 	= a[N-i-1][N-j-1];
 			a[N-i-1][N-j-1] = a[j][N-i-1];
 			a[j][N-i-1] = temp;
-
-			print_matrix(a, N);
-			printf("\n\n");
 		}
 	}
 
@@ -54,6 +62,13 @@ main()
 	int a[N][N] = {	{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
 	print_matrix(a, N);
+
+	printf("Right Rotate:\n");
 	right_rotate_90(a, N);
+	print_matrix(a, N);
+
+	printf("Left Rotate:\n");
+	left_rotate(a, N);
+	print_matrix(a, N);
 	return 0;
 }
