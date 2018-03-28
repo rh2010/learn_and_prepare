0fe6e3090697476733f9e47bb57861abbc80c31b
Rohit Hooda <hooda.rohit@gmail.com>
2018-03-28 12:51:03 -0700

    Adding some basic code for heaps.c

--- c/heaps/heaps.c
+++ c/heaps/heaps.c
@@ -18,8 +18,8 @@ struct heap {
 };
 
 // heap methods.
-struct heap *heap_init(int max_size);
-void heap_cleanup(void);
+struct heap *heap_init(struct heap *h, int max_size);
+void heap_cleanup(struct heap *h);
 int heap_insert(struct heap *h, int data);
 int heap_get_max(struct heap *h);
 int heap_peek_max(struct heap *h);
@@ -32,8 +32,20 @@ int heap_remove(struct heap *h, int index);
  * Initialize the heap
  */
 struct heap *
-heap_init(int max_size)
+heap_init(struct heap *h, int max_size)
 {
+    assert(h != NULL);
+
+    h->max_size = max_size;
+    h->size = 0;
+
+    // allocate memory for the heap data.
+    h->arr = (int *)malloc((sizeof (int)) * max_size);
+    if (h->arr == NULL) {
+        printf("Unable to allocate memory for the heap data\n");
+        exit(-1);
+    }
+
     return NULL;
 }
 
@@ -41,9 +53,12 @@ heap_init(int max_size)
  * Destroy the heap
  */
 void
-heap_cleanup(void)
+heap_cleanup(struct heap *h)
 {
+    assert(h != NULL);
 
+    free(h->arr);
+    h->size = h->max_size = 0;
 }
 
 /*
@@ -56,6 +71,7 @@ heap_cleanup(void)
 int
 heap_insert(struct heap *h, int data)
 {
+    assert(h != NULL);
     return -1;
 }
 
@@ -68,6 +84,7 @@ heap_insert(struct heap *h, int data)
 int
 heap_get_max(struct heap *h)
 {
+    assert(h != NULL);
     return 0;
 }
 
@@ -80,7 +97,16 @@ heap_get_max(struct heap *h)
 int
 heap_peek_max(struct heap *h)
 {
-    return 0;
+    int max;
+    assert(h != NULL);
+
+    if (h->size > 0) {
+        max = h->arr[0];
+    } else {
+        printf("Empty Heap\n");
+        max = -1;
+    }
+    return max;
 }
 
 /*
@@ -92,6 +118,7 @@ heap_peek_max(struct heap *h)
 void
 heap_sift_up(struct heap *h, int index)
 {
+    assert(h != NULL);
 
 }
 
@@ -104,6 +131,7 @@ heap_sift_up(struct heap *h, int index)
 void
 heap_sift_down(struct heap *h, int index)
 {
+    assert(h != NULL);
 
 }
 
@@ -114,7 +142,18 @@ heap_sift_down(struct heap *h, int index)
 void
 heap_print(struct heap *h)
 {
+    int i;
 
+    assert(h != NULL);
+
+    printf("Heap Dump:\n");
+    printf("\tMax Size: %d\n", h->max_size);
+    printf("\tSize: %d\n", h->size);
+    printf("\tData:\n\t");
+    for (i=0; i < h->size; i++) {
+        printf("%d ", h->arr[i]);
+    }
+    printf("\n");
 }
 
 /*
@@ -124,11 +163,43 @@ heap_print(struct heap *h)
 int
 heap_remove(struct heap *h, int index)
 {
+    assert(h != NULL);
     return 0;
 }
 
 int
 main(int argc, char** argv)
 {
+    int heap_size, range;
+    struct heap h;
+    // create a heap of size [user input]
+    if (argc != 3) {
+        printf("Usage: ./heap <max size of heap> <range for element value> \n");
+        return -1;
+    }
+    heap_size = atoi(argv[1]);
+    range = atoi(argv[2]);
+    // init the heap
+    heap_init(&h, heap_size);
+    //
+    // print the heap
+    heap_print(&h);
+    //
+    // randomly fill in the heap with size elements
+    //
+    // print the heap.
+    heap_print(&h);
+    //
+    // get the max elements
+    //
+    // print the heap
+    heap_print(&h);
+
+    // remove an element from the heap.
+    heap_remove(&h, 3);
+
+    // destroy the heap
+    heap_cleanup(&h);
+
 	return 0;
 }
