a43e7fe650dfbe8a759b9d8a88722abb26032bf9
Rohit Hooda <hooda.rohit@gmail.com>
2018-03-28 17:18:42 -0700

    [Heap Sort] Adding implementation of heap sort.
    Basic Algorithm for heap sort.
    
    - Given an array 'arr' of size 'size'.
    - Build a max heap from the array
    BuildMaxHeap(arr, s):
            for (i=size/2; i>=0;i--) {
                    sift_down(arr, i, s);
            }
    
    - repeat the following size-1 times.
    - Now the max element is at the '0' position.
    - Put it at the end and the element at the end of the array at the '0'th
      position.
    - reduce the size of the array by one i.e. ignore the max element for
      the next round of computation.
    - sift_down(arr, 0, size-1)

--- c/sorting/heap_sort.c
+++ c/sorting/heap_sort.c
@@ -1,15 +1,75 @@
 #include "../util.h"
 #include <time.h>
 
-void
-heap_sort(int *arr, int s)
-{
-    int size = s;
+#define PARENT(i) ((i)/2)
+#define LEFT_CHILD(i) ((2*(i)) + 1)
+#define RIGHT_CHILD(i) ((2*(i)) + 2)
 
-    for (i=0; i < size/2) {
-        sift_down()
+/*
+ * sift down
+ */
+void
+sift_down(int *arr, int index, int size)
+{
+    int l, r, next;
+
+    assert(arr != NULL);
+    assert(index <= size);
+
+    next = index;
+
+    l = LEFT_CHILD(index);
+    if (l < size && arr[l] > arr[next]) {
+        next = l;
+    }
+    r = RIGHT_CHILD(index);
+    if (r < size && arr[r] > arr[next]) {
+        next = r;
+    }
+
+    if (index != next) {
+        SWAP((arr+index), (arr+next));
+        sift_down(arr, next, size);
     }
 }
+
+/*
+ * From a given array of integers in random order,
+ * but, a max heap out of it ... in place.
+ */
+void
+heapify(int *arr, int s)
+{
+    int i, size = s;
+
+    for (i=size/2; i >= 0; i--) {
+        sift_down(arr, i, s);
+    }
+}
+
+void
+heap_sort(int *arr, int size)
+{
+    int i;
+
+    assert(arr != NULL);
+
+    // build a max-heap out of the array
+    heapify(arr, size);
+
+    int running_size = size;
+    while ((running_size - 1) > 0) {
+        // swap the first and the last element in the array
+        // the first element is the max element.
+        // so, place the first element in the last position.
+        SWAP((arr), (arr+running_size-1));
+        running_size = running_size - 1;
+        // bring the next highest max element of the array in the
+        // first position.
+        sift_down(arr, 0, running_size);
+    }
+}
+
 /*
  * Implement heap sort.
  */
@@ -26,7 +86,7 @@ main(int argc, char **argv)
 	}
 
 	count = atoi(argv[1]);
-	range = atoi(argv[1]);
+	range = atoi(argv[2]);
 
 	arr = (int *)malloc(sizeof (int) * count);
 	if (arr == NULL) {
