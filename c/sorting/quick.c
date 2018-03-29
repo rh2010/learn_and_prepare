a60853ef839e4b325ec9f3cef60594c8476a4429
Rohit Hooda <hooda.rohit@gmail.com>
2018-03-28 23:12:03 -0700

    [QuickSort] Adding a 2nd version of quick sort implementation. quick.c - chooses the first element of the array as the pivot element. quick_sort.c - chooses the 2nd element of the array as the pivot element.

---
+++ c/sorting/quick.c
@@ -0,0 +1,89 @@
+#include "../util.h"
+#include <time.h>
+
+/*
+ * 2nd version of quick sort
+ */
+int
+partition(int *arr, int s, int e)
+{
+	int i, j, p;
+
+	if (s >= e) {
+		return -1;
+	}
+	// i partition index
+	// j till where the partition is done
+	p = arr[s]; // let's use the first element as the pivot element to partition around
+	i = s+1;
+
+	for (j=s+1; j<=e;j++) {
+		if (arr[j] <= p) {
+			SWAP((arr+i), (arr+j));
+			i++;
+		}
+	}
+	SWAP((arr+s), (arr+i-1));
+
+	return i-1;
+}
+
+void
+quick(int *arr, int s, int e)
+{
+	int p_index;
+	printf("qs: s: %d, e: %d\n", s, e);
+	if  (s >= e) {
+		return;
+	}
+	p_index = partition(arr, s, e);
+
+	printf("Partition Index: [%d] %d\n", p_index, arr[p_index]);
+	print_int_arr(arr, e-s+1);
+	printf("\n");
+
+	quick(arr, s, p_index-1);
+	quick(arr, p_index+1, e);
+}
+
+int
+main(int argc, char **argv)
+{
+	int i = 0;
+	int count, range;
+	int *arr = NULL;
+
+	if (argc != 3) {
+		printf("usage: ./a.out <count of elements> <range >\n");
+		exit(-1);
+	}
+
+	count = atoi(argv[1]);
+	range = atoi(argv[2]);
+
+	arr = (int *)malloc(sizeof (int) * count);
+	if (arr == NULL) {
+		printf("Unable to allocate memory for the array\n");
+		return (-1);
+	}
+
+	// randomly fill the array
+    srand(time(NULL));
+
+	for (i = 0; i < count; i++) {
+		arr[i] = rand() % range;
+	}
+
+	printf("Input array\n");
+	print_int_arr(arr, count);
+
+	quick(arr, 0, count-1);
+
+	printf("Sorted array\n");
+	print_int_arr(arr, count);
+
+	// all done, free the memory
+	free(arr);
+
+	return 0;
+}
