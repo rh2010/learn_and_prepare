a4084b987a6d63291d4fb395d8674a530cdbe3a6
Rohit Hooda <hooda.rohit@gmail.com>
2018-04-02 23:04:32 -0700

    [Sets Union/Find] Adding unit tests for Set Union/Find implementation of rooted trees with rank rooted trees with rank with path compaction.

--- c/sets/sets.c
+++ c/sets/sets.c
@@ -33,6 +33,7 @@ struct set {
 void set_init(struct set *s, int size);
 void set_uninit(struct set *s);
 bool is_same_set(struct set *s, int key1, int key2);
+bool is_same_set_pc(struct set *s, int key1, int key2);
 void make_set(struct set *s, int key);
 int find(struct set *s, int key);
 void set_union(struct set *s, int key1, int key2);
@@ -153,6 +154,13 @@ is_same_set(struct set *s, int key1, int key2)
 	return (find(s, key1) == find(s, key2));
 }
 
+bool
+is_same_set_pc(struct set *s, int key1, int key2)
+{
+	assert(s != NULL);
+
+	return (find_pc(s, key1) == find_pc(s, key2));
+}
 /*
  * Perform a join of two sets.
  */
@@ -189,8 +197,7 @@ make_set_pc(struct set *s, int key)
 {
 	assert(s != NULL);
 
-	s->parent[key] = key;
-	s->rank[key] = 0;
+	make_set(s, key);
 }
 
 /*
@@ -215,9 +222,25 @@ find_pc(struct set *s, int key)
 void
 set_union_pc(struct set *s, int key1, int key2)
 {
+	int id1, id2;
 	assert(s != NULL);
 
-	set_union(s, key1, key2);
+	id1 = find_pc(s, key1);
+	id2 = find_pc(s, key2);
+
+	if (id1 == id2) {
+		// already in the same set
+		return;
+	}
+
+	if (s->rank[id1] > s->rank[id2]) {
+		s->parent[id2] = id1;
+	} else {
+		s->parent[id1] = id2;
+		if (s->rank[id1] == s->rank[id2]) {
+			s->rank[id2] += 1;
+		}
+	}
 }
 
 int
@@ -234,12 +257,112 @@ main(int argc, char **argv)
 	size = atoi(argv[1]);
 
 	// init the set
+	printf("Running tests for rooted tree sets\n");
 	set_init(&s, size);
 
 	set_build(&s);
 
 	set_dump(&s);
 
+	printf("Union (0, 2)\n");
+	set_union(&s, 0, 2);
+	printf("Are (0, 2) in same set [%s]\n", is_same_set(&s, 0, 2)?"True":"False");
+	printf("Are (1, 2) in same set [%s]\n", is_same_set(&s, 1, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (1, 5)\n");
+	set_union(&s, 1, 5);
+	printf("Are (1, 5) in same set [%s]\n", is_same_set(&s, 1, 5)?"True":"False");
+	printf("Are (0, 4) in same set [%s]\n", is_same_set(&s, 1, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (4, 7)\n");
+	set_union(&s, 4, 7);
+	printf("Are (4, 5) in same set [%s]\n", is_same_set(&s, 4, 5)?"True":"False");
+	printf("Are (7, 4) in same set [%s]\n", is_same_set(&s, 7, 4)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (9, 3)\n");
+	set_union(&s, 9, 3);
+	printf("Are (9, 3) in same set [%s]\n", is_same_set(&s, 9, 3)?"True":"False");
+	printf("Are (0, 3) in same set [%s]\n", is_same_set(&s, 0, 3)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (1, 2)\n");
+	set_union(&s, 1, 2);
+	printf("Are (1, 0) in same set [%s]\n", is_same_set(&s, 1, 0)?"True":"False");
+	printf("Are (2, 5) in same set [%s]\n", is_same_set(&s, 5, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (3, 4)\n");
+	set_union(&s, 3, 4);
+	printf("Are (9, 4) in same set [%s]\n", is_same_set(&s, 9, 4)?"True":"False");
+	printf("Are (7, 0) in same set [%s]\n", is_same_set(&s, 7, 0)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (8, 9)\n");
+	set_union(&s, 8, 9);
+	printf("Are (8, 3) in same set [%s]\n", is_same_set(&s, 8, 3)?"True":"False");
+	printf("Are (8, 7) in same set [%s]\n", is_same_set(&s, 8, 7)?"True":"False");
+	set_dump(&s);
+
+
 	// uinit a set
 	set_uninit(&s);
+	printf("Finishes running tests for rooted tree sets\n");
+
+	// init the set
+	printf("Running tests for rooted tree sets with path compaction\n");
+	set_init(&s, size);
+
+	set_build(&s);
+
+	set_dump(&s);
+
+	printf("Union (0, 2)\n");
+	set_union_pc(&s, 0, 2);
+	printf("Are (0, 2) in same set [%s]\n", is_same_set_pc(&s, 0, 2)?"True":"False");
+	printf("Are (1, 2) in same set [%s]\n", is_same_set_pc(&s, 1, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (1, 5)\n");
+	set_union_pc(&s, 1, 5);
+	printf("Are (1, 5) in same set [%s]\n", is_same_set_pc(&s, 1, 5)?"True":"False");
+	printf("Are (0, 4) in same set [%s]\n", is_same_set_pc(&s, 1, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (4, 7)\n");
+	set_union_pc(&s, 4, 7);
+	printf("Are (4, 5) in same set [%s]\n", is_same_set_pc(&s, 4, 5)?"True":"False");
+	printf("Are (7, 4) in same set [%s]\n", is_same_set_pc(&s, 7, 4)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (9, 3)\n");
+	set_union_pc(&s, 9, 3);
+	printf("Are (9, 3) in same set [%s]\n", is_same_set_pc(&s, 9, 3)?"True":"False");
+	printf("Are (0, 3) in same set [%s]\n", is_same_set_pc(&s, 0, 3)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (1, 2)\n");
+	set_union_pc(&s, 1, 2);
+	printf("Are (1, 0) in same set [%s]\n", is_same_set_pc(&s, 1, 0)?"True":"False");
+	printf("Are (2, 5) in same set [%s]\n", is_same_set_pc(&s, 5, 2)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (3, 4)\n");
+	set_union_pc(&s, 3, 4);
+	printf("Are (9, 4) in same set [%s]\n", is_same_set_pc(&s, 9, 4)?"True":"False");
+	printf("Are (7, 0) in same set [%s]\n", is_same_set_pc(&s, 7, 0)?"True":"False");
+	set_dump(&s);
+
+	printf("Union (8, 9)\n");
+	set_union_pc(&s, 8, 9);
+	printf("Are (8, 3) in same set [%s]\n", is_same_set_pc(&s, 8, 3)?"True":"False");
+	printf("Are (8, 7) in same set [%s]\n", is_same_set_pc(&s, 8, 7)?"True":"False");
+	set_dump(&s);
+
+
+	// uinit a set
+	set_uninit(&s);
+	printf("Finishes running tests for rooted tree sets with path compaction\n");
 }
