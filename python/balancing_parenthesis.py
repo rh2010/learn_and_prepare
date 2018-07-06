67027f7f6a3a2a5fe5de6f476f54e44d035ee663
Rohit Hooda <hooda.rohit@gmail.com>
2018-07-05 17:40:55 -0700

    [Make all possible sets of balanced parenthesis] Minor Fixes and cleanup.

--- python/balancing_parenthesis.py
+++ python/balancing_parenthesis.py
@@ -27,64 +27,60 @@ def solve(string):
     last_fix = 0 
     for i, e in enumerate(string):
         mismatch = False
-        print("Processing [%d]: %c" % (i, e))
+        #print("Processing [%d]: %c" % (i, e))
         if e is o:
             stack.append(e)
         if e is c:
             if len(stack) == 0:
+                # need to fix mismatch
                 mismatch = True
-                # fix mismatch
             else:
                 m = stack.pop()
                 if m != o:
                     mismatch = True
-            print("Stack: \t%s" % (stack))
+            #print("Stack: \t%s" % (stack))
 
             if mismatch:
-                print("Mismatch")
+                #print("Mismatch")
                 new_result = []
                 # fix mismatch
                 if len(result) == 0:
                     sub_str = list(string[last_fix:i+1])
-                    print(sub_str)
+                    #print(sub_str)
                     for ss_e_i, ss_e in enumerate(sub_str):
                         if ss_e is c:
-                            print("removing [%d] %c" % (ss_e_i, ss_e))
+                            #print("removing [%d] %c" % (ss_e_i, ss_e))
                             temp = list(sub_str)
                             temp.pop(ss_e_i)
-                            #temp.append(')')
-                            temps = str(''.join(temp))
-                            if temps not in result and temps not in new_result:
-                                print("Adding to result: %s" % (temps))
+                            temps = ''.join(temp)
+                            if temps not in new_result:
+                                #print("Adding to result: %s" % (temps))
                                 new_result.append(temps)
-                    result = result + new_result
                 else:
-                    print("Some strings already in result")
-                    print(len(result), result)
-                    new_result = []
+                    #print("Some strings already in result")
+                    #print(len(result), result)
                     for r in result:
-                        print("Processing %s" % (r))
+                        #print("Processing %s" % (r))
                         ss = string[last_fix:i+1]
-                        print("Since last fix: %s" % (ss))
+                        #print("Since last fix: %s" % (ss))
                         rs = r+ss
-                        print("Evaluating: %s" % (rs))
+                        #print("Evaluating: %s" % (rs))
                         for ss_e_i, ss_e in enumerate(rs):
                             if ss_e is c:
-                                print("removing [%d] %c" % (ss_e_i, ss_e))
+                                #print("removing [%d] %c" % (ss_e_i, ss_e))
                                 temp = list(rs)
                                 temp.pop(ss_e_i)
-                                #temp.append(')')
-                                temps = str(''.join(temp))
-                                print("Next result %s" % (temps))
+                                temps = ''.join(temp)
+                                #print("Next result %s" % (temps))
                                 if temps not in result and temps not in new_result:
-                                    print("Adding")
+                                    #print("Adding")
                                     new_result.append(temps)
-                                else:
-                                    print("Alread present")
+                                #else:
+                                #    print("Alread present")
                 last_fix = i+1
                 result = new_result
-                print("LastFix: %d" % (last_fix))
-                print("Result: \n\t%s" % (result))
+                #print("LastFix: %d" % (last_fix))
+                #print("Result: \n\t%s" % (result))
 
     return result
             
@@ -116,22 +112,21 @@ def validate_paren(string):
             if match != paren_pair[e]:
                 return False
             
-            
-    if len(stack) is 0:
-        return True
-    
-    return False
+    return (len(stack) == 0)
 
 if __name__=="__main__":
     s="([)]"
     s1="([])"
     s2="([]{}})"
     s3="()())())"
+    s4="))())())"
     
     #print(validate_paren(s))
     #print(validate_paren(s1))
     print(s3)
     print(solve(s3))
+    print(s4)
+    print(solve(s4))
     
     
 
